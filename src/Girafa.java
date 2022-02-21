d7dab52db6dc65f256b49e1b374b17de4738f48e
PetroviciMihail <mihail_petrovici@yahoo.com>
2022-02-21 11:12:21 +0200

    sem1 v2

---
+++ src/Girafa.java
@@ -0,0 +1,15 @@
+public class Girafa extends Animal {
+
+
+
+    public Girafa(String nume){
+        super(nume);
+    }
+
+
+
+    @Override
+    public void mananca(String tipMancare) {
+        System.out.println("girafa mananca "+ getNume()+ tipMancare);
+    }
+}
--- src/sem1.java
+++ src/sem1.java
@@ -1,5 +1,7 @@
 public class sem1 {
 
+
+
     public static void main(String[] args)
     {
         System.out.println("yes");
